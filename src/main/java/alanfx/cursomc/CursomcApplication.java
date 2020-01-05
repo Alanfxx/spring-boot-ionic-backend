fa3711ed6ecdce80f78384612c6a5ea3af7e041f
Alanfxx <j.alands2@gmail.com>
2020-01-05 14:34:02 -0300

    Estado e cidade

--- src/main/java/alanfx/cursomc/CursomcApplication.java
+++ src/main/java/alanfx/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import alanfx.cursomc.domain.Categoria;
+import alanfx.cursomc.domain.Cidade;
+import alanfx.cursomc.domain.Estado;
 import alanfx.cursomc.domain.Produto;
 import alanfx.cursomc.repositories.CategoriaRepository;
+import alanfx.cursomc.repositories.CidadeRepository;
+import alanfx.cursomc.repositories.EstadoRepository;
 import alanfx.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -41,7 +51,16 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
 	
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 	
 	
---
+++ src/main/java/alanfx/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package alanfx.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import alanfx.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
