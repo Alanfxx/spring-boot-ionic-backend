e370d52fd80c1c0079a08c7b746798e29ed4f6a8
Alanfxx <j.alands2@gmail.com>
2020-01-06 14:48:57 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/alanfx/cursomc/services/ClienteService.java
+++ src/main/java/alanfx/cursomc/services/ClienteService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import alanfx.cursomc.domain.Cliente;
+import alanfx.cursomc.dto.ClienteDTO;
 import alanfx.cursomc.repositories.ClienteRepository;
+import alanfx.cursomc.services.exceptions.DataIntegrityException;
 import alanfx.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,13 +22,48 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
-	public List<Cliente> buscarTodas(){
+	public List<Cliente> findAll(){
 		return repo.findAll();
+	}
+	
+	//paginação
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: "+id+", Tipo: "+Cliente.class.getName()));
+	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
 }
