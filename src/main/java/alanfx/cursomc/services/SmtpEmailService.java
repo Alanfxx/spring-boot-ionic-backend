b1baaafd0a8c94e53c26a10e1475c5b70f17f446
Alanfxx <j.alands2@gmail.com>
2020-01-07 23:47:07 -0300

    Email HTML

--- src/main/java/alanfx/cursomc/services/SmtpEmailService.java
+++ src/main/java/alanfx/cursomc/services/SmtpEmailService.java
@@ -1,12 +1,18 @@
 package alanfx.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService{
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 
 	@Autowired
 	private MailSender mailSender;
@@ -17,6 +23,13 @@ public class SmtpEmailService extends AbstractEmailService{
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Enviando email...");
 		mailSender.send(msg);
+		LOG.info("Email enviado");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando email...");
+		javaMailSender.send(msg);
 		LOG.info("Email enviado");
 	}
 }
