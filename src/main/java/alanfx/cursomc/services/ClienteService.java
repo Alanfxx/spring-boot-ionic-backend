73cbcc0a48ffdb92d9d0b4897f1e79937860c3fa
Alanfxx <j.alands2@gmail.com>
2020-01-10 12:12:56 -0300

    Ajustando tamanho da imagem

--- src/main/java/alanfx/cursomc/services/ClienteService.java
+++ src/main/java/alanfx/cursomc/services/ClienteService.java
@@ -46,6 +46,8 @@ public class ClienteService {
 	private ImageService imageService;
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+	@Value("${img.profile.size}")
+	private Integer size;
 	
 	
 	public List<Cliente> findAll(){
@@ -128,6 +130,9 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado");
 		}
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
